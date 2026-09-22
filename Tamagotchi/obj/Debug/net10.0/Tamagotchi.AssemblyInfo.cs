1f4f1ce3d6fdca9cfab9660daaf887a37934c2c0
Nills Maillet <mailletnills@gmail.com>
2026-09-22 10:19:49 +0200

    feat: CreationView interface as UseControl

--- Tamagotchi/obj/Debug/net10.0/Tamagotchi.AssemblyInfo.cs
+++ Tamagotchi/obj/Debug/net10.0/Tamagotchi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44a746df850920677471f9838be254558bcd422")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
