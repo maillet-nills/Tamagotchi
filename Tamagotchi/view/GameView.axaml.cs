f0fce84d8e53d7e06b3ba558274e66edfbd52ada
Nills Maillet <mailletnills@gmail.com>
2026-09-24 11:30:14 +0200

    branch: new branch Emagotchi

--- Tamagotchi/view/GameView.axaml.cs
+++ Tamagotchi/view/GameView.axaml.cs
@@ -1,17 +1,25 @@
+using System;
 using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using LibVLCSharp.Shared;
 
 namespace Tamagotchi.view;
 
 public partial class GameView : UserControl
 {
     private readonly GameViewModel gameViewModel;
+    private readonly LibVLC libVlc;
+    private MediaPlayer? lecteur;
     public GameView(string nom, Race race)
     {
+        Core.Initialize();
+        libVlc = new LibVLC();
+        
         InitializeComponent();
         gameViewModel = new GameViewModel(nom, race);
         DataContext = gameViewModel;
@@ -42,5 +50,23 @@ public partial class GameView : UserControl
     private void BTNDormir_OnClick(object? sender, RoutedEventArgs e)
     {
         gameViewModel.TamagotchiModel.AddSatisfaction();
+    }
+
+    private void BTNTuer_OnClick(object? sender, RoutedEventArgs e)
+    {
+        JouerSon("shot.mp3");
+        Tamago.Classes.Remove("balance");
+        Tamago.Classes.Add("tombe");
+    }
+    
+    private void JouerSon(string fichier)
+    {
+        var uri = new Uri($"avares://Tamagotchi/assets/{fichier}");
+        var flux = AssetLoader.Open(uri);
+        var media = new Media(libVlc, new StreamMediaInput(flux));
+
+        lecteur?.Dispose();
+        lecteur = new MediaPlayer(media);
+        lecteur.Play();
     }
 }
